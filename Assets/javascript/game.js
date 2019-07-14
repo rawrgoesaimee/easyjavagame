a998eb63aff658deded34d9c35bb7be68db8c1c4
rawrgoesaimee <rawrgoesaimee@hotmail.com>
2019-07-13 21:57:29 -0700

    Uploaded Assets and index html

---
+++ Assets/javascript/game.js
@@ -0,0 +1,73 @@
+let letters= ["a", "b",
+"c",
+"d",
+"e",
+"f",
+"g",
+"h",
+"i",
+"j",
+"k",
+"l",
+"m",
+"n",
+"o",
+"p",
+"q",
+"r",
+"s",
+"t",
+"u",
+"v",
+"w",
+"x",
+"y",
+"z"]
+
+let guessedLetters = [];
+let letterToGuess =null;
+let left = 9;
+let wins = 0;
+let losses = 0;
+
+function updateLetterToGuess(){
+    guessedLetters[Math.floor(Math.random() * letters.length)]; 
+} 
+
+function updateGuessesLeft(){
+    document.querySelector("#left").innerHTML = "Guesses left: " + left;
+}
+
+function updateGuessesSoFar(){
+    document.querySelector("#guesses").innerHtml = guessedLetters.join(",");
+}
+
+
+let reset = function(){
+    left = 9;
+    guessedLetters = [];
+    updateGuessesLeft();
+    updateLetterToGuess();
+    updateGuessesSoFar();
+
+    
+}
+document.onkeydown = function(event){
+    left--;
+
+    let letter = event.key.toLowerCase();
+    letters.push(letter);
+    updateGuessesLeft();
+    updateLetterToGuess();
+
+    if (letters === letterToGuess){
+            wins++;
+            document.querySelector("#wins").innerHTML = "wins: " + wins;
+            reset();
+    }
+    if (left === 0){
+        losses++;
+        document.querySelector("#losses").innerHTML = "Losses: " + losses;
+        reset();
+    }
+};
